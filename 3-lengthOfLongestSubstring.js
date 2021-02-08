88e0bf48628e9af6c0fd75e6e71c34e9514baf0e
sun <876538934@qq.com>
2021-02-08 15:49:04 +0800

    master

---
+++ 3-lengthOfLongestSubstring.js
@@ -0,0 +1,28 @@
+var s = "pwwkew";
+
+var lengthOfLongestSubstring = function(s) {
+    var set = new Set();
+    var i = 0;
+    var j = 0;
+    var maxLength = 0;
+    if(s.length === 0){
+        return 0;
+    }
+
+    for(i;i < s.length;i++){
+        if(!set.has(s[i])){
+            set.add(s[i]);
+            maxLength = Math.max(maxLength,set.size);
+        }else{
+            while(set.has(s[i])){
+                set.delete(s[j]);
+                j++;
+            }
+            set.add(s[i]);
+        }
+    }
+
+    return maxLength;
+};
+
+console.log(lengthOfLongestSubstring(s));
