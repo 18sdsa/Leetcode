ff9571d054494150ae54dbeec88047338e80d6ba
sun <876538934@qq.com>
2021-02-15 13:33:07 +0800

    master

--- leetcode/6-convert.js
+++ leetcode/6-convert.js
@@ -2,15 +2,28 @@ var s = "PAYPALISHIRING";
 var numRows = 3;
 
 /**
- * PAYP
- *      ALIS
- *           HIRI
- *              NG 
  * 
- * PAHNAPLSIIGYIR
  * @param {*} s 
  * @param {*} numRows 
  */
 var convert = function(s, numRows) {
-
+    var index;
+    var step = 2*numRows-2;//第0行和第n行的间距
+    var res = "";
+    for(var i = 0; i < numRows; i++) {
+        var index = i;
+        var add = 2 * i;
+        while(index < s.length) {
+            res = res + s[index];
+            add = step - add;
+            if(i == 0 || i == numRows-1) {
+                index = index + step;
+            }else{
+                index = index + add;
+            }
+        }
+    }
+    return res;
 };
+
+console.log(convert(s,numRows))
